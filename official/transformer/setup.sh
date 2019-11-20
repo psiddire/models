2724779fe27a89cdd1430725d899c20e9b786dd9
Prasanna Siddireddy <psiddire@crcfe02.crc.nd.edu>
2019-11-19 20:36:40 -0500

    Setup and Data

---
+++ official/transformer/setup.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH="$PYTHONPATH:/afs/crc.nd.edu/user/p/psiddire/Attention/models"
+PARAM_SET=big
+DATA_DIR=$HOME/Attention/models/official/transformer/data
+MODEL_DIR=$HOME/Attention/models/official/transformer/model_$PARAM_SET
+VOCAB_FILE=$DATA_DIR/vocab.ende.32768
